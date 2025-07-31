1654c2fe7022c6b5ecec4669783c6b1d6d7190e7
HyunsangJoo <joohs86@naver.com>
2025-07-31 16:59:19 +0900

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
-<<<<<<< Updated upstream
-export default defineConfig({
-  base: "/practice_tools/",
-  plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: 'dashes',
-      generateScopedName: '[name]__[local]___[hash:base64:5]',
-    },
-  },
-});
-=======
 export default defineConfig(({ command }) => {
   const config = {
     plugins: [react()],
@@ -31,4 +19,3 @@ export default defineConfig(({ command }) => {
 
   return config;
 });
->>>>>>> Stashed changes
